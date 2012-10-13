48dbfd5b147d62ea09aa2b1e7cf5baf2de835212
Matthew Sorensen <matthew.d.sorensen@gmail.com>
2012-10-13 15:15:57 -0700

    Slight performance improvement.
    Shaves 4% of the time to deduplicate/write a set of points by avoiding
    allocating an intermediate list of vertices.

--- Graphics/Implicit/Export/Additive.hs
+++ Graphics/Implicit/Export/Additive.hs
@@ -6,9 +6,7 @@ import Graphics.Implicit.Definitions
 import Data.Hashable
 import Data.HashMap.Strict
 import Prelude hiding (lookup,mapM)
-import Control.Monad.State hiding (mapM)
-import Data.Sequence 
-import Data.Traversable (mapM)
+import Control.Monad.State
 import Data.Monoid
 import Text.Blaze.Internal
 import Text.Blaze (toMarkup)
@@ -26,14 +24,14 @@ instance Hashable Point3 where
 data Tri = Tri !Int !Int !Int
          deriving(Show,Eq)
 
-type VSet = (HashMap Point3 (Point3,Int), Int, Seq (Point3,Point3))
+type VSet = (HashMap Point3 Int, Int, Markup)
 
 fromℝ3 :: ℝ3 -> Point3
 fromℝ3 (x,y,z) = P3 x y z
 
-deduplicate :: [NormedTriangle] -> ([Tri], Seq (Point3,Point3))
+deduplicate :: [NormedTriangle] -> ([Tri], (Int,Markup))
 deduplicate = shuffle . flip runState (mempty,0,mempty) . mapM lookupTriangle
-    where shuffle (triangles,(_,_,verts)) = (triangles,verts)
+    where shuffle (triangles,(_,u,verts)) = (triangles,(u,verts))
           lookupTriangle :: NormedTriangle -> State VSet Tri
           lookupTriangle (a,b,c) = do
             a' <- lookupNormedPoint a
@@ -42,33 +40,34 @@ deduplicate = shuffle . flip runState (mempty,0,mempty) . mapM lookupTriangle
             return $! Tri a' b' c'
 
 lookupNormedPoint :: (ℝ3,ℝ3) -> State VSet Int
-lookupNormedPoint (p,n) = do
+lookupNormedPoint (p,normal) = do
   let point = fromℝ3 p 
   (hash,uni,l) <- get
   case lookup point hash of
-    Just (_,i)  -> return i
+    Just i  -> return i
     Nothing -> do
-      let normal = fromℝ3 n
-      put $! (insert point (normal,uni) hash, uni+1, l |> (point,normal))
+      put $! (insert point uni hash, uni+1, Append (vertex point normal) l)
       return uni
 
 parent t x = customParent (textTag t) x
 
-xt p =  Parent "x" "<x" "</x>" $ toMarkup p
-yt p =  Parent "y" "<y" "</y>" $ toMarkup p
-zt p =  Parent "z" "<z" "</z>" $ toMarkup p
+xt p =  Parent "x" "<x" "</x>" $ Content $ PreEscaped $ String $ show p
+yt p =  Parent "y" "<y" "</y>" $ Content $ PreEscaped $ String $ show p
+zt p =  Parent "z" "<z" "</z>" $ Content $ PreEscaped $ String $ show p
 
-xnt p =  Parent "nx" "<nx" "</nx>" $ toMarkup p
-ynt p =  Parent "ny" "<ny" "</ny>" $ toMarkup p
-znt p =  Parent "nz" "<nz" "</nz>" $ toMarkup p
+xnt p =  Parent "nx" "<nx" "</nx>" $ Content $ PreEscaped $ String $ show p
+ynt p =  Parent "ny" "<ny" "</ny>" $ Content $ PreEscaped $ String $ show p
+znt p =  Parent "nz" "<nz" "</nz>" $ Content $ PreEscaped $ String $ show p
 
-v1t p = Parent "v1" "<v1" "</v1>" $ toMarkup p
-v2t p = Parent "v2" "<v2" "</v2>" $ toMarkup p
-v3t p = Parent "v3" "<v3" "</v3>" $ toMarkup p
+v1t p = Parent "v1" "<v1" "</v1>" $ Content $ PreEscaped $ String $ show p
+v2t p = Parent "v2" "<v2" "</v2>" $ Content $ PreEscaped $ String $ show p
+v3t p = Parent "v3" "<v3" "</v3>" $ Content $ PreEscaped $ String $ show p
 
-vertices :: Seq (Point3,Point3) -> Markup
-vertices = parent "vertices" . fmap (const ()) . mapM vertex
-    where vertex ((P3 x y z),(P3 nx ny nz))
+vertices :: Markup -> Markup
+vertices = parent "vertices" 
+
+vertex :: Point3 -> ℝ3 -> Markup  
+vertex (P3 x y z) (nx,ny,nz)
               = parent "vertex" $ do
                   parent "coordinates" $ do
                                     xt x
@@ -79,20 +78,20 @@ vertices = parent "vertices" . fmap (const ()) . mapM vertex
                                     ynt ny
                                     znt nz        
 
-volume :: [Tri] -> Markup
-volume = parent "volume" . mapM_ triangle
+volume :: [Tri] -> Int -> Markup
+volume tr u = parent "volume" $ mapM_ triangle tr
     where triangle (Tri a b c) 
               = parent "triangle" $ do
-                  v1t a
-                  v2t b
-                  v3t c
+                  v1t $ u - a
+                  v2t $ u - b
+                  v3t $ u - c
 
-
-file :: [Tri] -> Seq (Point3,Point3) -> Markup
-file tri verts = 
+file :: [Tri] -> (Int,Markup) -> Markup
+file tri (u,verts) = 
     parent "amf" $ parent "object" $ parent "mesh" $ do
       vertices verts
-      volume tri
+      volume tri $ u-1
 
 amf :: [NormedTriangle] -> ByteString
 amf = compress . renderMarkup . uncurry file . deduplicate
+
