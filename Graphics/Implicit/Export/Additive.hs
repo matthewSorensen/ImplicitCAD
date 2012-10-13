a2b72c1b33d1cfdc6713a7b766a737eb17d245ae
Matthew Sorensen <matthew.d.sorensen@gmail.com>
2012-10-13 16:42:16 -0700

    Turns out we don't need to be careful with strict things

--- Graphics/Implicit/Export/Additive.hs
+++ Graphics/Implicit/Export/Additive.hs
@@ -3,45 +3,31 @@
 module Graphics.Implicit.Export.Additive where
 
 import Graphics.Implicit.Definitions
-import Data.Hashable
 import Data.HashMap.Strict
-import Prelude hiding (lookup,mapM)
+import Prelude hiding (lookup)
 import Control.Monad.State
-import Data.Monoid
+import Data.Monoid (mempty)
 import Text.Blaze.Internal
 import Text.Blaze (toMarkup)
-import Text.Blaze.Renderer.Utf8
+import Text.Blaze.Renderer.Utf8 (renderMarkup)
 import Data.ByteString.Lazy.Char8 (ByteString)
 import Codec.Compression.GZip (compress)
 
-data Point3 = P3 !Float !Float !Float
-              deriving(Show,Eq)
+type Tri = (Int,Int,Int)
+type VSet = (HashMap ℝ3 Int, Int, Markup)
 
-instance Hashable Point3 where
-    hash (P3 x y z) = hash x `hashWithSalt` y `hashWithSalt` z
-    hashWithSalt s (P3 x y z) = s `hashWithSalt` x `hashWithSalt` y `hashWithSalt` z
-
-data Tri = Tri !Int !Int !Int
-         deriving(Show,Eq)
-
-type VSet = (HashMap Point3 Int, Int, Markup)
-
-fromℝ3 :: ℝ3 -> Point3
-fromℝ3 (x,y,z) = P3 x y z
-
-deduplicate :: [NormedTriangle] -> ([Tri], (Int,Markup))
+deduplicate :: [NormedTriangle] -> ([Tri],Int,Markup)
 deduplicate = shuffle . flip runState (mempty,0,mempty) . mapM lookupTriangle
-    where shuffle (triangles,(_,u,verts)) = (triangles,(u,verts))
+    where shuffle (triangles,(_,u,verts)) = (triangles,u-1,verts)
           lookupTriangle :: NormedTriangle -> State VSet Tri
           lookupTriangle (a,b,c) = do
             a' <- lookupNormedPoint a
             b' <- lookupNormedPoint b
             c' <- lookupNormedPoint c
-            return $! Tri a' b' c'
+            return (a',b',c')
 
 lookupNormedPoint :: (ℝ3,ℝ3) -> State VSet Int
-lookupNormedPoint (p,normal) = do
-  let point = fromℝ3 p 
+lookupNormedPoint (point,normal) = do
   (hash,uni,l) <- get
   case lookup point hash of
     Just i  -> return i
@@ -63,11 +49,15 @@ v1t p = Parent "v1" "<v1" "</v1>" $ Content $ PreEscaped $ String $ show p
 v2t p = Parent "v2" "<v2" "</v2>" $ Content $ PreEscaped $ String $ show p
 v3t p = Parent "v3" "<v3" "</v3>" $ Content $ PreEscaped $ String $ show p
 
+
+-- z y x nx ny nz v1 v2 v3
+-- vertex coordinates normal
+
 vertices :: Markup -> Markup
 vertices = parent "vertices" 
 
-vertex :: Point3 -> ℝ3 -> Markup  
-vertex (P3 x y z) (nx,ny,nz)
+vertex :: ℝ3 -> ℝ3 -> Markup  
+vertex (x,y,z) (nx,ny,nz)
               = parent "vertex" $ do
                   parent "coordinates" $ do
                                     xt x
@@ -80,18 +70,17 @@ vertex (P3 x y z) (nx,ny,nz)
 
 volume :: [Tri] -> Int -> Markup
 volume tr u = parent "volume" $ mapM_ triangle tr
-    where triangle (Tri a b c) 
+    where triangle (a,b,c) 
               = parent "triangle" $ do
                   v1t $ u - a
                   v2t $ u - b
                   v3t $ u - c
 
-file :: [Tri] -> (Int,Markup) -> Markup
-file tri (u,verts) = 
+file :: ([Tri],Int,Markup) -> Markup
+file (tri,u,verts) = 
     parent "amf" $ parent "object" $ parent "mesh" $ do
       vertices verts
-      volume tri $ u-1
+      volume tri u
 
 amf :: [NormedTriangle] -> ByteString
-amf = compress . renderMarkup . uncurry file . deduplicate
-
+amf = compress . renderMarkup . file . deduplicate
